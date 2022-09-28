2adacb22462659806d85a16328bac564609f4c1e
Donald Pakkies <donaldpakkies@gmail.com>
2022-09-28 13:18:57 +0200

    feat: export service resolver

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { TypeScriptPortsServiceResolver } from './TypeScriptPortsServiceResolver';
 import { use } from './Decorators/use';
 
 export {
+    TypeScriptPortsServiceResolver,
     use
 };
